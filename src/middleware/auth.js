72b46fc90dc600de4c2ba85c2006200ab6eb62a5
sharjun hussain <sharjoonibnuhussain99@gmail.com>
2026-05-02 10:27:40 +0530

    feat: import Organization model into authentication middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { User, Role, Permission, Branch, Employee } = require('../models');
+const { User, Role, Permission, Branch, Employee, Organization } = require('../models');
 
 /**
  * Authentication Middleware
