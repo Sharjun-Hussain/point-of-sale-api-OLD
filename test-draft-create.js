06d90279e5f8c61150fd9159e730a0112768ee45
Sharjun-Hussain <sharjoonibnuhussain99@gmail.com>
2026-07-10 00:49:30 +0530

    refactor: remove legacy testing scripts and documentation from old project directory

--- test-draft-create.js
+++
@@ -1,8 +0,0 @@
-const db = require('./src/models');
-db.sequelize.sync({ alter: true }).then(() => {
-    console.log('Sync successful');
-    process.exit(0);
-}).catch(e => {
-    console.error('Sync failed', e);
-    process.exit(1);
-});
--- test-draft.js
+++
@@ -1,9 +0,0 @@
-const db = require('./src/models');
-console.log('UserDraft exists:', !!db.UserDraft);
-db.sequelize.sync({ alter: true }).then(() => {
-    console.log('Sync successful');
-    db.sequelize.close();
-}).catch(e => {
-    console.error('Sync failed', e);
-    db.sequelize.close();
-});
